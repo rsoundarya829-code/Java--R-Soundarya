843af9bce704d0a458174efa1964e8b542bbd724
R Soundarya <rsoundarya829@gmail.com>
2026-05-21 21:25:20 +0530

    Initial Commit

---
+++ Operators.java
@@ -0,0 +1,35 @@
+public class Operators {
+    public static void main(String args[]){
+        //Arithmetic Operators
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b;
+        int quotient = a / b;
+        int modulus = a % b;  
+        System.out.println("Sum: " + sum);  
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+        System.out.println("Modulus: " + modulus);
+        //Relations Operators
+        int x = 5;
+        int y = 10;
+        boolean isEqual = (x == y);
+        boolean isNotEqual = (x != y);
+        boolean isGreaterThan = (x > y);
+        boolean isLessThan = (x < y);
+        boolean isGreaterThanOrEqual = (x >= y);
+        boolean isLessThanOrEqual = (x <= y);   
+        System.out.println("Is Equal: " + isEqual);
+        System.out.println("Is Not Equal: " + isNotEqual);
+        System.out.println("Is Greater Than: " + isGreaterThan);
+        System.out.println("Is Less Than: " + isLessThan);
+        System.out.println("Is Greater Than Or Equal: " + isGreaterThanOrEqual);
+        System.out.println("Is Less Than Or Equal: " + isLessThanOrEqual);
+
+    }
+
+    
+}
