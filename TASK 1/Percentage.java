3d8a4b172133bd784d5b836f9a1e657ba8eb0ce6
R Soundarya <rsoundarya829@gmail.com>
2026-05-24 16:42:50 +0530

    code

---
+++ TASK 1/Percentage.java
@@ -0,0 +1,11 @@
+public class Percentage {
+    public static void main(String args[]){
+        int totalscore = 500;
+        int actualscore = 400;
+        int percentage = (actualscore * 100) / totalscore;
+        double per1 = (double)percentage;
+        System.out.println("Percentage:"+per1);
+    }
+
+    
+}
