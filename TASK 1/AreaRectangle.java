b6bafcdc1951595fa600ffc2957721e61c324793
R Soundarya <rsoundarya829@gmail.com>
2026-05-24 16:00:16 +0530

    Calculating area

---
+++ TASK 1/AreaRectangle.java
@@ -0,0 +1,9 @@
+public class AreaRectangle {
+    public static void main(String args[]){
+        int length = 20;
+        int breadth = 10;
+        int area = length * breadth;
+        System.out.println("AREA OF RECTANGLE:"+area);
+    }
+    
+}
