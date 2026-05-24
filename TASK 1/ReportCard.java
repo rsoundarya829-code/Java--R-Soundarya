ed79a41766c9577fc5f7bcc9150d29fb57741c75
R Soundarya <rsoundarya829@gmail.com>
2026-05-24 16:30:03 +0530

    code

---
+++ TASK 1/ReportCard.java
@@ -0,0 +1,12 @@
+public class ReportCard {
+
+    public static void main(String args[]){
+        int studentID = 123;
+        double score = 90.5;
+        char grade = 'A';
+        System.out.println("Student ID:"+studentID);
+        System.out.println("Score:"+score);
+        System.out.println("Grade:"+grade);
+    }   
+    
+}
