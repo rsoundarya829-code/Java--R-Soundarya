f033b00cf8e2a45ddb8063d730edc5986c54363c
R Soundarya <rsoundarya829@gmail.com>
2026-05-24 16:20:36 +0530

    code

---
+++ TASK 1/Ascii.java
@@ -0,0 +1,9 @@
+public class Ascii {
+    public static void main(String args[]){
+        char character = 'E';
+        int asciivalue = (int) character;
+        System.out.println("character:"+character);
+        System.out.println("ASCII value:"+asciivalue);
+    }
+    
+}
