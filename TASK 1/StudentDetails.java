5a42b28a9395f2b4c3945da54f7537b635a6ee74
R Soundarya <rsoundarya829@gmail.com>
2026-05-24 15:23:09 +0530

    Fix: remove nested git repo from TASK 1

---
+++ TASK 1/StudentDetails.java
@@ -0,0 +1,14 @@
+
+
+public class StudentDetails {
+    public static void main(String args[]){
+        int age = 20;
+        String name = "Ram";
+        String course = "CSE";
+        System.out.println("Name:"+name);
+        System.out.println("Age:"+age);
+        System.out.println("Course:"+course);
+
+
+    }
+}
