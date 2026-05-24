f3530705f7b3622604d9088661ced45190fe16e7
R Soundarya <rsoundarya829@gmail.com>
2026-05-24 15:47:55 +0530

    details

--- TASK 1/StudentDetails.java
+++ TASK 1/StudentDetails.java
@@ -5,9 +5,11 @@ public class StudentDetails {
         int age = 20;
         String name = "Ram";
         String course = "CSE";
+        int usn = 123;
         System.out.println("Name:"+name);
         System.out.println("Age:"+age);
         System.out.println("Course:"+course);
+        System.out.println("USN:"+usn);
 
 
     }
