ea0aee9eb8ec6be83bff8f3a18fbcadb89a82f9f
R Soundarya <rsoundarya829@gmail.com>
2026-05-24 16:14:54 +0530

    variables

---
+++ TASK 1/Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void main(String args[]){
+       int a = 10;
+       char b = 'B';
+       float c = 3.14f;
+       boolean d = true;
+       System.out.println("int value:"+a);
+       System.out.println("char value:"+b);
+       System.out.println("float value:"+c);
+       System.out.println("boolean value:"+d);
+       
+
+
+    }
+    
+}
