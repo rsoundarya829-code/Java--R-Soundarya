ea2de04091b3618127ef3e96413eb51d96499045
R Soundarya <rsoundarya829@gmail.com>
2026-05-26 13:30:45 +0530

    code

---
+++ oper.java
@@ -0,0 +1,11 @@
+public class oper {
+    public static void main(String args[]) {
+
+        int a = 10;
+        int b = 5;
+
+        System.out.println(a > b && b != 0);
+        System.out.println(a < b || a == 10);
+        System.out.println(!(a == b));
+    }
+}
