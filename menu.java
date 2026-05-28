a32a01050942e4a7bce076d6284f16781b46c433
R Soundarya <rsoundarya829@gmail.com>
2026-05-28 13:10:08 +0530

    Menu

---
+++ menu.java
@@ -0,0 +1,17 @@
+public class menu {
+    public static void main(String args[]){
+        int choice = 2;
+        switch(choice){
+            case 1: System.out.println("dosa");
+            break;
+            case 2: System.out.println("idly");
+            break;
+            case 3: System.out.println("poori");
+            break;
+            case 4: System.out.println("julebi");
+            break;
+            default:System.out.println("Invalid choice");
+        }
+    }
+    
+}
