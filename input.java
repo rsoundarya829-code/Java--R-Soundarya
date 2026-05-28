1099e9fb843ab0ae40690b9432808e27f36f7491
R Soundarya <rsoundarya829@gmail.com>
2026-05-28 13:09:42 +0530

    input statement

---
+++ input.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+
+public class input {
+    public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    int a =sc.nextInt();
+    float b = sc.nextFloat();
+    char v = sc.next().charAt(0);
+    System.out.println("int number is:  "+a);
+    System.out.println("float number is:  "+b);
+    System.out.println("charcter is:  "+v);
+
+    
+}
+}
