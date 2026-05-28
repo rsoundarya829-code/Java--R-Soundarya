11228940b10bbc3dbe9610365eb15b89b8b050d2
R Soundarya <rsoundarya829@gmail.com>
2026-05-28 13:09:03 +0530

    code

---
+++ conditional.java
@@ -0,0 +1,13 @@
+public class conditional {
+    public static void main(String args[]){
+        int a = 40;
+        if(a>60){
+            System.out.println("PASS");
+        }
+        else {
+            System.out.println("FAIL");
+        }
+        }
+
+    }
+
