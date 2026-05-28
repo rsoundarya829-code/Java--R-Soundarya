daee7b4b30d02bf3ab5db4ffc8f4d0190ce5a3a3
R Soundarya <rsoundarya829@gmail.com>
2026-05-28 13:10:51 +0530

    signals

---
+++ traffic.java
@@ -0,0 +1,18 @@
+public class traffic {
+    public static void main(String args[]){
+        String signal = "green";
+        if(signal=="red")
+        {
+            System.out.println("STOP");
+        }
+        else if(signal=="yellow")
+        {
+            System.out.println("wait");
+        }
+        else{
+            System.out.println("Go");
+        }
+        }
+    }
+    
+
