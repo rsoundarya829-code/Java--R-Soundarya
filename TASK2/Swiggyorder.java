207813c46482c6dbb1a2490d85b59ec7e1d7340b
R Soundarya <rsoundarya829@gmail.com>
2026-05-28 12:59:09 +0530

    Swiggy order

---
+++ TASK2/Swiggyorder.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Swiggyorder {
+    public static void main(String args[]) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter the food item :");
+        String item = sc.nextLine();
+        System.out.println("Enter the quantity :");
+        int quantity = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Enter the Address :");
+        String Address = sc.nextLine();
+        System.out.println("Order placed");
+       
+    }
+}
