917acd04a49c6c5d77208a0a53faaad0c87986ce
R Soundarya <rsoundarya829@gmail.com>
2026-05-30 21:16:02 +0530

    using break statement

---
+++ breakexample.java
@@ -0,0 +1,14 @@
+public class breakexample {
+    public static void main(String args[]){
+        String Studentsname[] = {"rolex","reena","brownly"};
+        for(int i=0;i<Studentsname.length;i++){
+            if (Studentsname[i].equals("reena")) {
+        
+        System.out.println("name found "+ Studentsname[i]);
+        break;
+        }
+    }
+        }
+    }   
+    
+
